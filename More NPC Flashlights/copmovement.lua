f3b81aa7bd772e7bf1950b7230dfc926d8616279
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2022-08-28 14:38:53 +0100

    Update copmovement.lua

--- More NPC Flashlights/copmovement.lua
+++ More NPC Flashlights/copmovement.lua
@@ -2,8 +2,4 @@ Hooks:PostHook(CopMovement, "set_cool", "flashlight_set_cool", function(self, st
 	if self._ext_inventory:equipped_unit() then
 		self._ext_inventory:equipped_unit():base():set_flashlight_enabled(not state) -- enable the flashlight when we become uncool, or disable if we become cool again
 	end
-	
-	local my_right = Vector3()
-
-	mrotation.x(self:m_rot(), my_right)
 end)
